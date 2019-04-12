65b3c0d5831c60c0730ebf74740086ecf24f213a
Yusuf Pamukçu <yusufpamukcu@yandex.com>
2019-04-12 11:35:40 +0300

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/yusuf/filmbilgileri/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/yusuf/filmbilgileri/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.imakeanapp.imapmovies", appContext.getPackageName());
+        assertEquals("com.yusuf.filmbilgileri", appContext.getPackageName());
     }
 }
